021fcc8bd042bfe1cb50a1c6a1ea44471b169e07
Romain Vincent <romainfvincent@gmail.com>
2024-12-14 13:29:03 +0100

    conf(conform): update format providers (prettier, prettier everywhere)

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -12,11 +12,13 @@ return {
                 lua        = { "stylua",       lsp_format = "never"    },
                 go         = { "gofmt",        lsp_format = "never"    },
                 rust       = { "rustfmt",      lsp_format = "never"    },
+                json       = { "prettier",     lsp_format = "never"    },
+                javascript = { "prettier",     lsp_format = "fallback" },
                 typescript = { "prettier",     lsp_format = "fallback" },
                 vue        = { "prettier",     lsp_format = "fallback" },
                 markdown   = { "prettier",     lsp_format = "fallback" },
                 html       = { "prettier",     lsp_format = "fallback" },
-                svg        = { "xmlformatter", lsp_format = "fallback" }
+                svg        = { "prettier",     lsp_format = "fallback" }
             },
         })
 
