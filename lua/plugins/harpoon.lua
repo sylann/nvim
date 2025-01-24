efd66337438cf2b2f2913cd5bb7e8201325e76ab
Romain Vincent <romainfvincent@gmail.com>
2025-01-24 14:32:27 +0100

    conf(harpoon): adapt keymaps (changed keyboard layout again)

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -10,14 +10,14 @@ return {
 
         harpoon:setup({})
 
-        vim.keymap.set("n", "<leader>@", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-        vim.keymap.set("n", "<leader>#", function() harpoon:list():add() end)
+        vim.keymap.set("n", "<leader>_", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set("n", "<leader>=", function() harpoon:list():add() end)
 
         vim.keymap.set("n", "<leader>&", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<leader>|", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<leader>é", function() harpoon:list():select(2) end)
         vim.keymap.set("n", '<leader>"', function() harpoon:list():select(3) end)
         vim.keymap.set("n", "<leader>'", function() harpoon:list():select(4) end)
-        vim.keymap.set("n", "<leader>(", function() harpoon:list():select(5) end)
-        vim.keymap.set("n", "<leader>)", function() harpoon:list():select(6) end)
+        vim.keymap.set("n", "<leader>|", function() harpoon:list():select(5) end)
+        vim.keymap.set("n", "<leader>ù", function() harpoon:list():select(6) end)
     end,
 }
