d319fa6943bfb536fc843ce1a16e0f8d7d04774d
Romain Vincent <romainfvincent@gmail.com>
2025-01-24 14:45:39 +0100

    feat: add plugin "render-markdown.nvim" to prettify markdown editing

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -6,4 +6,19 @@ return {
         ft = { "markdown" },
         build = ":call mkdp#util#install()",
     },
+
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+
+        dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+
+        config = function()
+            require("render-markdown").setup({
+                bullet = { left_pad = 1 },
+                code = { width = "block", min_width = 60 },
+                heading = { enabled = true, position = "inline", backgrounds = {} },
+                sign = { enabled = false },
+            })
+        end,
+    },
 }
