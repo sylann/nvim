f975e60e230ee981f47eba4dabd70b46c0786c6c
Romain Vincent <romainfvincent@gmail.com>
2025-01-27 23:08:09 +0100

    conf(cmp): improve sorting in autocomplete menu (especially in python)

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -16,6 +16,20 @@ return {
         local icon_by_kind = require("icons").icon_by_symbol_kind
         local snippy = require("snippy")
 
+        ---offset: Entries prefixed with "_" will be ranked lower, and "__" even lower.
+        ---Symbols prefixed with "_" are considered private by convention in many languages.
+        ---@type cmp.ComparatorFunction
+        local function underscore_prefixes(entry1, entry2)
+            local _1, _2 = entry1.word:sub(1, 1) == "_", entry2.word:sub(1, 1) == "_"
+            if _1 ~= _2 then return _2 end
+            if _1 then
+                local __1, __2 = entry1.word:sub(2, 2) == "_", entry2.word:sub(2, 2) == "_"
+                if __1 ~= __2 then return __2 end
+            end
+
+            return nil
+        end
+
         cmp.setup({
             snippet = {
                 expand = function(args) snippy.expand_snippet(args.body) end,
@@ -39,6 +53,23 @@ return {
                 ["<Enter>"] = cmp.mapping.confirm({ select = true }),
                 ["<C-Space>"] = cmp.mapping.complete({}),
             }),
+
+            sorting = {
+                priority_weight = 2,
+                comparators = {
+                    underscore_prefixes,
+                    cmp.config.compare.offset,
+                    -- cmp.config.compare.exact,
+                    cmp.config.compare.scopes,
+                    cmp.config.compare.score,
+                    -- cmp.config.compare.recently_used,
+                    cmp.config.compare.locality,
+                    cmp.config.compare.kind,
+                    -- cmp.config.compare.sort_text,
+                    cmp.config.compare.length,
+                    cmp.config.compare.order,
+                },
+            },
 
             sources = {
                 { name = "nvim_lsp" },
