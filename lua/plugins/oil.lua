e6b8b6e7eba012488899710db46cff8fc96fe90e
Romain Vincent <romainfvincent@gmail.com>
2025-01-27 23:05:38 +0100

    conf(oil): tweak keymaps (don't use defaults)

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -21,11 +21,10 @@ return {
             },
 
             -- INFO: Keymaps — Only from within Oil
+            use_default_keymaps = false,
             keymaps = {
                 ["g?"] = actions.show_help,
                 ["<CR>"] = actions.select,
-                ["<C-s>"] = actions.select_vsplit,
-                ["<C-h>"] = actions.select_split,
                 ["<C-t>"] = actions.select_tab,
                 ["<C-p>"] = actions.preview,
                 ["<C-c>"] = actions.close,
