5de46c56487f8366fd0bcdf84fe189071eb55540
Romain Vincent <romainfvincent@gmail.com>
2025-01-27 23:02:11 +0100

    conf: reduce max number of "floating" lines for parent scopes

--- lua/plugins/treesitter-context.lua
+++ lua/plugins/treesitter-context.lua
@@ -3,4 +3,9 @@ return {
     --   When parent scopes start beyond currently visible lines,
     --   this plugin shows them as virtual lines floating at the screen top.
     "nvim-treesitter/nvim-treesitter-context",
+
+    opts = {
+        max_lines = 10,
+        multiline_threshold = 1,
+    },
 }
