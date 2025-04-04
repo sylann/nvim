904dd86c1770dcbc2ce4bdecbdeae0946b3c5fc9
Romain Vincent <romainfvincent@gmail.com>
2025-04-04 03:35:45 +0200

    conf(colorizer): fix colorizer not properly reset on colorscheme change

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,10 +1,21 @@
+local function setup()
+    local colorizer = require("colorizer")
+    colorizer.setup({ "*", "!git", "!fugitive" }, { mode = "foreground" })
+    return colorizer
+end
+
 return {
     -- Add syntax color to written colors (hex, rgb, common names, ...)
     "norcalli/nvim-colorizer.lua",
 
     config = function()
-        require("colorizer").setup({ "*", "!git", "!fugitive" }, { mode = "foreground" })
+        setup()
 
-        vim.keymap.set("n", "<leader>co", vim.cmd.ColorizerToggle)
+        vim.api.nvim_create_autocmd("ColorScheme", {
+            callback = function()
+                package.loaded["colorizer"] = nil
+                setup().attach_to_buffer(0)
+            end,
+        })
     end,
 }
