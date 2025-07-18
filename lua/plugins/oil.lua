3b582689e72e8d0da60eeec5f7bbd4df93bc2ec7
Romain Vincent <romainfvincent@gmail.com>
2025-07-18 19:49:40 +0200

    feat(oil): copy path of current entry (prefer relative to workspace)

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,3 +1,29 @@
+local function yank_entry_path(regname)
+    return function()
+        local oilpath = vim.api.nvim_buf_get_name(0)
+        local oil_prefix = "oil://"
+
+        if not oilpath:starts_with(oil_prefix) then
+            vim.notify(string.format("Unexpected Oil path: %s", oilpath), vim.log.levels.ERROR)
+            return
+        end
+
+        local ws_prefix = vim.fn.getcwd()
+
+        -- Try path relative to workspace, fallback to absolute path
+        local prefix = oil_prefix .. ws_prefix .. "/"
+        if not oilpath:starts_with(prefix) then prefix = oil_prefix end
+
+        -- Contruct path of entry
+        local dir_path = string.sub(oilpath, #prefix + 1)
+        local entry_name = require("oil").get_cursor_entry().name
+        local entry_path = dir_path .. entry_name
+
+        vim.notify(entry_path, vim.log.levels.INFO)
+        vim.fn.setreg(regname, entry_path)
+    end
+end
+
 return {
     "stevearc/oil.nvim",
     enabled = true,
@@ -37,6 +63,8 @@ return {
                 ["gx"] = actions.open_external,
                 ["g."] = actions.toggle_hidden,
                 ["gt"] = actions.toggle_trash,
+                ["<C-y>"] = yank_entry_path("@"),
+                ["<leader>y"] = yank_entry_path("+"),
             },
         })
 
