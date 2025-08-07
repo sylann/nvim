9eedb965ff88578585205c640e3c29457b8dc18c
Romain Vincent <romainfvincent@gmail.com>
2025-08-07 16:33:30 +0200

    feat(oil): add ability to change root directory and select previous ones

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,3 +1,71 @@
+local root_dirs = { vim.fn.getcwd() }
+local root_dir_menu_bufnr = nil
+
+local function rdm_select_entry()
+    local dir = vim.api.nvim_get_current_line()
+    vim.cmd({ cmd = "cd", args = { dir } })
+    vim.api.nvim_win_close(0, true)
+    vim.notify(string.format("CWD: %s", dir), vim.log.levels.INFO)
+end
+
+local function rdm_delete_entry()
+    if #root_dirs == 1 then return end
+    local row_index = vim.api.nvim_win_get_cursor(0)[1]
+    table.remove(root_dirs, row_index)
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, root_dirs)
+end
+
+local function rdm_close() vim.api.nvim_win_close(0, true) end
+
+-- TODO: extract an util function for this recurring need (create_floating_window), need to figure out good defaults
+
+---@param buf integer
+---@return integer
+local function create_floating_window(buf)
+    local width = math.min(80, math.floor(vim.o.columns * 0.8))
+    local height = math.min(#root_dirs, math.floor(vim.o.lines * 0.8))
+    local win = vim.api.nvim_open_win(buf, true, {
+        relative = "win",
+        border = "rounded",
+        width = width,
+        height = height,
+        row = math.floor(((vim.o.lines - height) / 2) - 1),
+        col = math.floor((vim.o.columns - width) / 2),
+    })
+    vim.api.nvim_set_option_value("number", false, { win = win })
+    vim.api.nvim_set_option_value("relativenumber", false, { win = win })
+    return win
+end
+
+local function rdm_open()
+    local buf = root_dir_menu_bufnr
+    if not buf then
+        buf = vim.api.nvim_create_buf(false, true)
+        vim.api.nvim_buf_set_name(buf, "oil://root-dir-history")
+        root_dir_menu_bufnr = buf
+    end
+
+    create_floating_window(buf)
+    vim.api.nvim_buf_set_lines(buf, 0, -1, false, root_dirs)
+    vim.api.nvim_set_option_value("modified", false, { buf = buf })
+
+    vim.api.nvim_buf_set_keymap(buf, "n", "d", "", { callback = rdm_delete_entry })
+    vim.api.nvim_buf_set_keymap(buf, "n", "q", "", { callback = rdm_close })
+    vim.api.nvim_buf_set_keymap(buf, "n", "<Esc>", "", { callback = rdm_close })
+    vim.api.nvim_buf_set_keymap(buf, "n", "<Enter>", "", { callback = rdm_select_entry })
+end
+
+local function cd_with_history()
+    local dir = require("oil").get_current_dir()
+    if dir then
+        vim.cmd({ cmd = "cd", args = { dir } })
+        if not vim.tbl_contains(root_dirs, dir) then table.insert(root_dirs, dir) end
+        vim.notify(string.format("CWD: %s", dir), vim.log.levels.INFO)
+    else
+        vim.notify("Cannot :cd; not in a directory", vim.log.levels.WARN)
+    end
+end
+
 local function yank_entry_path(regname)
     return function()
         local oilpath = vim.api.nvim_buf_get_name(0)
@@ -57,8 +125,9 @@ return {
                 ["<C-c>"] = actions.close,
                 ["<C-l>"] = actions.refresh,
                 ["µ"] = actions.parent,
-                ["_"] = actions.open_cwd,
-                ["-"] = actions.cd,
+                ["="] = actions.open_cwd,
+                ["-"] = cd_with_history,
+                ["_"] = rdm_open,
                 ["gs"] = actions.change_sort,
                 ["gx"] = actions.open_external,
                 ["g."] = actions.toggle_hidden,
