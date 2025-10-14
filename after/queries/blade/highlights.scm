73f5870f3a10bff196a4d050828faa0810de5eca
Romain Vincent <romainfvincent@gmail.com>
2025-10-14 15:15:18 +0200

    wip: fix treesitter queries
    Without the extend directive, nothing works at all.
    There must be some important queries in the base declarations.

--- after/queries/blade/highlights.scm
+++ after/queries/blade/highlights.scm
@@ -1,9 +1,11 @@
-(directive) @function.macro
-(directive_start) @function.macro
-(directive_end) @function.macro
-(comment) @comment
-((parameter) @include (#set! "priority" 110)) 
-((php_only) @include (#set! "priority" 110)) 
-((bracket_start) @function (#set! "priority" 120)) 
-((bracket_end) @function (#set! "priority" 120)) 
-(keyword) @keyword
+; extends
+
+; ((directive) @function.macro)
+; ((directive_start) @function.macro)
+; ((directive_end) @function.macro)
+; ((comment) @comment)
+; ((parameter) @include (#set! "priority" 110)) 
+; ((php_only) @include (#set! "priority" 110)) 
+; ((bracket_start) @function (#set! "priority" 120)) 
+; ((bracket_end) @function (#set! "priority" 120)) 
+; ((keyword) @keyword)
